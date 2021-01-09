8600a26cc7d88b6abc52125b283b5e1462c6d075
jakarin_a <jakarin_a@softsquaregroup.com>
2021-01-09 13:40:38 +0700

    delete comment

--- Form1.cs
+++ Form1.cs
@@ -257,16 +257,6 @@ namespace WindowsFormsApp2
 
         private void Calculate()
         {
-            //     true  && true  = true
-            //     true  && false = false
-            //     false && false = false
-
-            //     true  || true  = true
-            //     true  || false = true
-            //     false || false = false
-
-            //     !true = false
-            //     !false = true
             if (textBox1.Text == "0" && operate == "/")
             {
                 MessageBox.Show("ไม่สามารถหารด้วย 0 ได้");
